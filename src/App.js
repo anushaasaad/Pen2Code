129b6aaee90b69179e92bb709c76c46126d998c8
Anusha Saad <62885127+anushaasaad@users.noreply.github.com>
2023-02-09 01:20:03 +0500

    main

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Routes } from 'react-router-dom'
 import Main from './Components/Main/main.js';
 import Upload from './Components/upload/upload.js';
 import Logo from './logo.PNG';
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
     <div className="App">
-    <Upload />
-    <Routes>
-      <Route path="/" element={Main} />
-      <Route path="/Try" element={Upload} />
-     
+      <Routes>
+      <Route path="/"  element={<Main />} />
+      <Route path="/Upload" element={<Upload />} />
       </Routes>
-  </div>
-  </Router>
+          </div>
+    </BrowserRouter>
+
   );
 }
 
--- src/Components/upload/upload.js
+++ src/Components/upload/upload.js
@@ -1,10 +1,18 @@
 import React from "react";
 import '../upload/style.css';
 import Header from "../header/header.js";
+import ImageSlider from './slider.js';
+import { SliderData } from './sliderdata.js'
+
 function Upload() {
   return (
     <div class="Upload">
         <Header />
+        <div className="sec">
+            <div className="max-width">
+                <ImageSlider slides={SliderData} />
+                 </div>
+            </div>
         </div>
   );
 }
